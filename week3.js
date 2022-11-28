d770b545143d83e2f7902fe57a31e802f3c8f873
Mai Vang <vmaineng@gmail.com>
2022-11-27 16:10:34 -0800

    almost there

--- week3.js
+++ week3.js
@@ -1,18 +1,28 @@
 //problem 1: Frequency Counter Pattern
 
-//const nums = (arr) => {
+const solution = (nums) => {
 //step 1: edge cases
+//if (!nums.length) return -1;
 
+//step 2: initalize objects to keep track of the counts
+let count1 = {};
 
-//step 2: 
+//step 3: iterate through the elements in array and update to the object.
+//check the frequency of each number in the array, if it exists(key), the value will increase by one
+//if it does not exist, we will keep the value as one
+for (let num of nums) {
+    count1[num] = (count1[num] || 0) + 1
+}
 
+for (let key in count1) {
+    //check if the key occurred more than once
+    if (key > (nums.length/2)) return key
+}
+//if no main element, return -1
+//return -1
+}
 
-//step 3: 
-
-
-//}
-
-
+console.log(solution([2,2,1,1,1,2,2]))
 
 //problem 2: Divide and Conquer Pattern
 
@@ -22,36 +32,32 @@
 //time complexity: O( log n)
 //space complexity: O(1) ; returning an integer
 
-const binaryIterativeSearch = (nums, target) => {
-//step 1: edge cases
-if (!nums.length) return -1
+// const binaryIterativeSearch = (nums, target) => {
+  //step 1: edge cases
+//   if (!nums.length) return -1;
 
-//step 2: initalize variables
-let start = 0;
-let end = nums.length - 1;
+  //step 2: initalize variables
+//   let start = 0;
+//   let end = nums.length - 1;
 
-//create a divider
-//return middle of this is the target we're looking for
+//   while (start < end) {
+    //create a divider
+    // let middleIndex = Math.floor((start + end) / 2);
+    //return middle of this is the target we're looking for
+    // if (nums[middleIndex] === target) {
+    //   return middleIndex;
+      //else start < target
+    // } else if (nums[middleIndex] < target) {
+    //   start = middleIndex + 1;
+      //else start > target
+//     } else {
+//       end = middleIndex - 1;
+//     }
+//   }
 
-while (start < end) {
-    let middleIndex = Math.floor((start + end)/2)
+//   return -1;
 
-    if (nums[middleIndex] === target) {
-        return middleIndex;
-    } else if (nums[middleIndex] < target) {
-        start = middleIndex + 1;
-    } else {
-        end = middleIndex - 1;
-    }
-}
+  //else return -1
+//};
 
-return - 1
-//else start < end
-
-//else start > end
-
-//else return -1
-
-}
-
-console.log(binaryIterativeSearch([-1,0,3,5,9,12], 2))
+//console.log(binaryIterativeSearch([-1, 0, 3, 5, 9, 12], 2));
