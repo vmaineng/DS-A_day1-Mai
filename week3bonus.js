c662f421027b23dc0bb42a499ff557c7c419f747
Mai Vang <vmaineng@gmail.com>
2022-11-28 16:55:16 -0800

    setup

---
+++ week3bonus.js
@@ -0,0 +1,19 @@
+//problem 1 - binary search
+
+//brute force method
+
+//const triangleNumber = (nums) => {
+
+//}
+
+//console.log(triangleNumber([2,2,3,4]))
+
+//problem 2 - frequency counter pattern
+
+//brute force method
+
+//const findDuplicates = (nums) => {
+
+//}
+
+//console.log(findDuplicates([4,3,2,7,8,2,3,1]]))
