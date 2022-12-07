385af04fcf754cc8a30defc7a0c1c39d40e481d0
Mai Vang <vmaineng@gmail.com>
2022-12-07 14:28:17 -0800

    added answers

--- week4.js
+++ week4.js
@@ -1,56 +1,53 @@
 //problem 1 recursion
 
+const flatTheArray = (arr) => {
 
-//const flatTheArray = (arr) => {
-
-//step 1 edge cases
-//if (!arr.length) return -1;
+//step 1 edge cases - if array is empty, return - 1;
+if (!arr.length) return -1;
 
 //step 2 
 //initialize the array
-
-//let results = [];
+let results = [];
 
 //for...loop iterate through the array
 
-// for (let i = 0; i < arr.length; i++) {
-//     if(Array.isArray(arr[i])) {
-//         results = results.concat(flatTheArray(arr[i]));
-//     } else {
-//         results.push(arr[i]);
-//     }
-// }
-// return results;
-// }
+for (let i = 0; i < arr.length; i++) {
+    if(Array.isArray(arr[i])) {
+        results = results.concat(flatTheArray(arr[i]));
+    } else {
+        results.push(arr[i]);
+    }
+}
+return results;
+}
 
-// console.log(flatTheArray([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]));
+console.log(flatTheArray([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]));
 
 
 //problem 2 recursion
+const capitalizeAllLetters = (array) => {
 
-// const capitalizeAllLetters = (array) => {
-
-//edge cases
-// if (!array.length) return -1;
+//edge cases - if array is empty, return - 1;
+ if (!array.length) return -1;
 
 //base case - exit the loop
-// if (array.length === 1) return [array[0].toUpperCase()];
+ if (array.length === 1) return [array[0].toUpperCase()];
 
 //recursive path - recall the function
-// let results = capitalizeAllLetters(array.slice(0, -1));
-// results.push(array.slice(array.length - 1)[0].toUpperCase());
-// return results;
+let results = capitalizeAllLetters(array.slice(0, -1));
+results.push(array.slice(array.length - 1)[0].toUpperCase());
+return results;
 
-// }
+}
 
-// console.log(capitalizeAllLetters(['mai', 'vang']));
+ console.log(capitalizeAllLetters(['mai', 'vang']));
 
 //problem 3 recursion
 
 //iterative method
 function factorial(x){
 //step 1 edge cases - if x is not a number, return -1
-//if (typeof x !== NaN) return -1;  --still figure this part out
+if (typeof x !== 'number') return - 1;
 
 //initialize sum
 let sum = 1;
@@ -72,7 +69,7 @@ console.log(factorial(7))
 //recursive approach
 function factorial(x) {
 //edge case - if x is not a number, return -1
-if (x !== NaN) return -1;
+if (typeof x !== 'number') return - 1;
 
 //base case - exit the loop
 if (!x) return 1;
@@ -83,82 +80,83 @@ return x * factorial(x-1);
 
 console.log(factorial(5))
 
+
 //problem 4 recursion
 function collectStrings(obj) {
 
 //initalize an array
-// let results = [];
+ let results = [];
 
 //edge case = if object is empty, return -1
-// if (!Object.keys(obj).length) return -1;
+ if (!Object.keys(obj).length) return -1;
 
 //base case - exit the loop
-// for (let key in obj){
-//     if (typeof obj[key] === 'string'){
-//         results.push(obj[key]);
-//     } else if (typeof obj[key] === 'object') {
-//             results = results.concat(collectStrings(obj[key])) //need to avoid objects b/c the 2 turned into a -1, which we do not want
-//         }
-//     }
+for (let key in obj){
+    if (typeof obj[key] === 'string'){
+        results.push(obj[key]);
+    } else if (typeof obj[key] === 'object') {
+            results = results.concat(collectStrings(obj[key])) //need to avoid objects b/c the 2 turned into a -1, which we do not want
+        }
+    }
 
-// return results;
-// }
+return results;
+}
 
-// console.log(collectStrings({a: "foo", b: 2, c: "bar"}));
+ console.log(collectStrings({a: "foo", b: 2, c: "bar"}));
 
 //problem 5 bubble sort
 
-// const data = [ 
-//     { 
-//         name: 'John Smith',   
-//         age: new Map([['age', 88]]),   
-//         favoriteMovie: [  
-//             {  
-//                 title: 'Hulk',   
-//                 genre: 'action',   
-//                 rating: 6  
-//             }  
-//         ]  
-//     },  
-//     {  
-//         name: 'Tony Kim',   
-//         age: new Map([['age', 3]]),   
-//         favoriteMovie: [  
-//             {  
-//                 title: 'Top Gun',   
-//                 genre: 'action',   
-//                 rating: 10  
-//             }  
-//         ]  
-//     },  
-//     {  
-//         name: 'John Smith',   
-//         age: new Map([['age', 35]]),   
-//         favoriteMovie: [  
-//             {  
-//                 title: 'Saw',   
-//                 genre: 'horror',   
-//                 rating: 8  
-//             }  
-//         ]  
-//     }  
-// ]
+const data = [ 
+    { 
+        name: 'John Smith',   
+        age: new Map([['age', 88]]),   
+        favoriteMovie: [  
+            {  
+                title: 'Hulk',   
+                genre: 'action',   
+                rating: 6  
+            }  
+        ]  
+    },  
+    {  
+        name: 'Tony Kim',   
+        age: new Map([['age', 3]]),   
+        favoriteMovie: [  
+            {  
+                title: 'Top Gun',   
+                genre: 'action',   
+                rating: 10  
+            }  
+        ]  
+    },  
+    {  
+        name: 'John Smith',   
+        age: new Map([['age', 35]]),   
+        favoriteMovie: [  
+            {  
+                title: 'Saw',   
+                genre: 'horror',   
+                rating: 8  
+            }  
+        ]  
+    }  
+]
 
 
-// const bubbleSort = (arr) => {
-//     for(let i = arr.length-1 ; i > 0; i--) {
-//         for(let j = 0; j<i; j++) {
-//             if(arr[j].age.get('age') > arr[j + 1].age.get('age')){
-//                 let temp = arr[j];
-//                 arr[j] = arr[j + 1];
-//                 arr[j + 1] = temp;
-//             }
-//         }
-//     }
-//     return arr.reverse();
-// }
+const bubbleSort = (arr) => {
+    for(let i = arr.length-1 ; i > 0; i--) {
+        for(let j = 0; j<i; j++) {
+            if(arr[j].age.get('age') > arr[j + 1].age.get('age')){
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr.reverse();
+}
 
-// console.log(bubbleSort(data))
+console.log(bubbleSort(data))
 
 
 
