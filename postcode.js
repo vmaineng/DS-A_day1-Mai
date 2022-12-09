50e852cec3d75eb9e6e3e8a7a589839589711461
Mai Vang <vmaineng@gmail.com>
2022-12-09 15:02:26 -0800

    capitalizeFirstLetter fini

---
+++ postcode.js
@@ -0,0 +1,179 @@
+{
+    results: [
+    {
+    gender: "male",
+    name: {
+    title: "Mr",
+    first: "Caleb",
+    last: "Jennings"
+    },
+    location: {
+    street: {
+    number: 630,
+    name: "Spring St"
+    },
+    city: "Syracuse",
+    state: "Tennessee",
+    country: "United States",
+    postcode: 79897,
+    coordinates: {
+    latitude: "89.3077",
+    longitude: "-135.7856"
+    },
+    timezone: {
+    offset: "+5:30",
+    description: "Bombay, Calcutta, Madras, New Delhi"
+    }
+    },
+    email: "caleb.jennings@example.com",
+    login: {
+    uuid: "27b1cedc-6dd4-40da-b7f0-ca72de1e8282",
+    username: "yellowbear824",
+    password: "jjjjjjjj",
+    salt: "A12rb1OR",
+    md5: "d53353c24b594edfe5eea94da8664e8f",
+    sha1: "5db858dc4186b49d00c284c86123d4ebe583e9fd",
+    sha256: "f1ce43cf616638295f0365de0eadaf394718f1935c3de0aa5c4a553cc1898c3a"
+    },
+    dob: {
+    date: "1981-04-07T20:04:39.175Z",
+    age: 41
+    },
+    registered: {
+    date: "2012-04-05T08:35:14.411Z",
+    age: 10
+    },
+    phone: "(881) 951-5190",
+    cell: "(892) 231-8696",
+    id: {
+    name: "SSN",
+    value: "765-31-1986"
+    },
+    picture: {
+    large: "https://randomuser.me/api/portraits/men/13.jpg",
+    medium: "https://randomuser.me/api/portraits/med/men/13.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/men/13.jpg"
+    },
+    nat: "US"
+    },
+    {
+    gender: "female",
+    name: {
+    title: "Miss",
+    first: "Emma",
+    last: "Morrison"
+    },
+    location: {
+    street: {
+    number: 5810,
+    name: "New Street"
+    },
+    city: "Leicester",
+    state: "Tyne and Wear",
+    country: "United Kingdom",
+    postcode: "Y6 3RQ",
+    coordinates: {
+    latitude: "83.3807",
+    longitude: "-178.5450"
+    },
+    timezone: {
+    offset: "+4:30",
+    description: "Kabul"
+    }
+    },
+    email: "emma.morrison@example.com",
+    login: {
+    uuid: "de3162ea-eaba-4963-afda-6b5a4a987e0f",
+    username: "ticklishcat484",
+    password: "leslie",
+    salt: "7R5SUkVJ",
+    md5: "6e24d17ee15e56d841b1ff1bece3723b",
+    sha1: "42ff9ea38d87d86cbc58676905ddde964653ca40",
+    sha256: "087e3f034b1cc0a91652768bfa9fef110109149147777137383d8454ef98f6cf"
+    },
+    dob: {
+    date: "1982-06-19T15:18:08.435Z",
+    age: 40
+    },
+    registered: {
+    date: "2012-08-09T22:08:20.110Z",
+    age: 10
+    },
+    phone: "01370 87284",
+    cell: "07195 011834",
+    id: {
+    name: "NINO",
+    value: "CA 52 11 39 A"
+    },
+    picture: {
+    large: "https://randomuser.me/api/portraits/women/38.jpg",
+    medium: "https://randomuser.me/api/portraits/med/women/38.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
+    },
+    nat: "GB"
+    },
+    {
+    gender: "female",
+    name: {
+    title: "Miss",
+    first: "Harriet",
+    last: "Patel"
+    },
+    location: {
+    street: {
+    number: 3499,
+    name: "West Coast Road"
+    },
+    city: "Upper Hutt",
+    state: "Tasman",
+    country: "New Zealand",
+    postcode: 93051,
+    coordinates: {
+    latitude: "37.7404",
+    longitude: "149.4523"
+    },
+    timezone: {
+    offset: "-3:30",
+    description: "Newfoundland"
+    }
+    },
+    email: "harriet.patel@example.com",
+    login: {
+    uuid: "bb2c98d2-6a86-45c1-ab4d-655484936f78",
+    username: "tinyswan263",
+    password: "mindy",
+    salt: "OT1m5pxo",
+    md5: "cab00ae54c8c376a9c2c83429fb4ca27",
+    sha1: "827d0d5529d23b9d8d5085eecf0528164b53a2c3",
+    sha256: "9b7caff598986a5eedd7702777eabbcc78193edf4512c9ee5d9855afc5775fb3"
+    },
+    dob: {
+    date: "1992-02-18T20:15:45.328Z",
+    age: 30
+    },
+    registered: {
+    date: "2002-07-03T11:35:15.967Z",
+    age: 20
+    },
+    phone: "(120)-361-0221",
+    cell: "(647)-364-0124",
+    id: {
+    name: "",
+    value: null
+    },
+    picture: {
+    large: "https://randomuser.me/api/portraits/women/13.jpg",
+    medium: "https://randomuser.me/api/portraits/med/women/13.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/women/13.jpg"
+    },
+    nat: "NZ"
+    }
+    ],
+
+    // info: {
+    // seed: "1c71f1144dc84145",
+    // results: 3,
+    // page: 1,
+    // version: "1.4"
+    // }
+    }
