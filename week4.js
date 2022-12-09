1d3e9ef332bf90d6958c4948ab49f7dbb4b24b4b
Mai Vang <vmaineng@gmail.com>
2022-12-09 10:03:40 -0800

    iterative

--- week4.js
+++ week4.js
@@ -1,173 +1,162 @@
 //problem 1 recursion
 
 const flatTheArray = (arr) => {
+  //step 1 edge cases - if array is empty, return - 1;
+  if (!arr.length) return -1;
 
-//step 1 edge cases - if array is empty, return - 1;
-if (!arr.length) return -1;
+  //step 2
+  //initialize the array
+  let results = [];
 
-//step 2 
-//initialize the array
-let results = [];
+  //for...loop iterate through the array
 
-//for...loop iterate through the array
-
-for (let i = 0; i < arr.length; i++) {
-    if(Array.isArray(arr[i])) {
-        results = results.concat(flatTheArray(arr[i]));
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      results = results.concat(flatTheArray(arr[i]));
     } else {
-        results.push(arr[i]);
+      results.push(arr[i]);
     }
-}
-return results;
-}
+  }
+  return results;
+};
 
 console.log(flatTheArray([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]));
 
-
 //problem 2 recursion
 const capitalizeAllLetters = (array) => {
+  //edge cases - if array is empty, return - 1;
+  if (!array.length) return -1;
 
-//edge cases - if array is empty, return - 1;
- if (!array.length) return -1;
+  //base case - exit the loop
+  if (array.length === 1) return [array[0].toUpperCase()];
 
-//base case - exit the loop
- if (array.length === 1) return [array[0].toUpperCase()];
+  //recursive path - recall the function
+  let results = capitalizeAllLetters(array.slice(0, -1));
+  results.push(array.slice(array.length - 1)[0].toUpperCase());
+  return results;
+};
 
-//recursive path - recall the function
-let results = capitalizeAllLetters(array.slice(0, -1));
-results.push(array.slice(array.length - 1)[0].toUpperCase());
-return results;
-
-}
-
- console.log(capitalizeAllLetters(['mai', 'vang']));
+console.log(capitalizeAllLetters(["mai", "vang"]));
 
 //problem 3 recursion
 
 //iterative method
-function factorial(x){
-//step 1 edge cases - if x is not a number, return -1
-if (typeof x !== 'number') return - 1;
+function factorial(x) {
+  //step 1 edge cases - if x is not a number, return -1
+  if (typeof x !== "number") return -1;
 
-//initialize sum
-let sum = 1;
+  //initialize sum
+  let sum = 1;
 
-//base case
-if (sum === 2) {
-    return sum = 2;
+  //base case
+  if (sum === 2) {
+    return (sum = 2);
+  }
+
+  //iterate through numbers; not positions; to get the previous number and keep multiplying
+  for (let i = 2; i <= x; i++) {
+    sum = sum * i;
+  }
+  return sum;
 }
 
-//iterate through numbers; not positions; to get the previous number and keep multiplying
-for (let i = 2; i <= x; i++){
-  sum =  sum * i;
-}
-return sum;
-}
-
-console.log(factorial(7))
+console.log(factorial(7));
 
 //recursive approach
 function factorial(x) {
-//edge case - if x is not a number, return -1
-if (typeof x !== 'number') return - 1;
+  //edge case - if x is not a number, return -1
+  if (typeof x !== "number") return -1;
 
-//base case - exit the loop
-if (!x) return 1;
+  //base case - exit the loop
+  if (!x) return 1;
 
-//recursive case - to recall the function
-return x * factorial(x-1);
+  //recursive case - to recall the function
+  return x * factorial(x - 1);
 }
 
-console.log(factorial(5))
-
+console.log(factorial(5));
 
 //problem 4 recursion
 function collectStrings(obj) {
+  //initalize an array
+  let results = [];
 
-//initalize an array
- let results = [];
+  //edge case = if object is empty, return -1
+  if (!Object.keys(obj).length) return -1;
 
-//edge case = if object is empty, return -1
- if (!Object.keys(obj).length) return -1;
-
-//base case - exit the loop
-for (let key in obj){
-    if (typeof obj[key] === 'string'){
-        results.push(obj[key]);
-    } else if (typeof obj[key] === 'object') {
-            results = results.concat(collectStrings(obj[key])) //need to avoid objects b/c the 2 turned into a -1, which we do not want
-        }
+  //base case - exit the loop
+  for (let key in obj) {
+    if (typeof obj[key] === "string") {
+      results.push(obj[key]);
+    } else if (typeof obj[key] === "object") {
+      results = results.concat(collectStrings(obj[key])); //need to avoid objects b/c the 2 turned into a -1, which we do not want
     }
+  }
 
-return results;
+  return results;
 }
 
- console.log(collectStrings({a: "foo", b: 2, c: "bar"}));
+console.log(collectStrings({ a: "foo", b: 2, c: "bar" }));
 
 //problem 5 bubble sort
 
-const data = [ 
-    { 
-        name: 'John Smith',   
-        age: new Map([['age', 88]]),   
-        favoriteMovie: [  
-            {  
-                title: 'Hulk',   
-                genre: 'action',   
-                rating: 6  
-            }  
-        ]  
-    },  
-    {  
-        name: 'Tony Kim',   
-        age: new Map([['age', 3]]),   
-        favoriteMovie: [  
-            {  
-                title: 'Top Gun',   
-                genre: 'action',   
-                rating: 10  
-            }  
-        ]  
-    },  
-    {  
-        name: 'John Smith',   
-        age: new Map([['age', 35]]),   
-        favoriteMovie: [  
-            {  
-                title: 'Saw',   
-                genre: 'horror',   
-                rating: 8  
-            }  
-        ]  
-    }  
-]
-
+const data = [
+  {
+    name: "John Smith",
+    age: new Map([["age", 88]]),
+    favoriteMovie: [
+      {
+        title: "Hulk",
+        genre: "action",
+        rating: 6,
+      },
+    ],
+  },
+  {
+    name: "Tony Kim",
+    age: new Map([["age", 3]]),
+    favoriteMovie: [
+      {
+        title: "Top Gun",
+        genre: "action",
+        rating: 10,
+      },
+    ],
+  },
+  {
+    name: "John Smith",
+    age: new Map([["age", 35]]),
+    favoriteMovie: [
+      {
+        title: "Saw",
+        genre: "horror",
+        rating: 8,
+      },
+    ],
+  },
+];
 
 const bubbleSort = (arr) => {
-    for(let i = arr.length-1 ; i > 0; i--) {
-        for(let j = 0; j<i; j++) {
-            if(arr[j].age.get('age') > arr[j + 1].age.get('age')){
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j].age.get("age") > arr[j + 1].age.get("age")) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
-    return arr.reverse();
-}
+  }
+  return arr.reverse();
+};
 
-console.log(bubbleSort(data))
-
-
-
+console.log(bubbleSort(data));
 
 /// just for fun ---- please ignore bottom part;
 //data.forEach((dat) => (console.log(dat.age.values()))) //grabbed age in an object
 
 // data.forEach((dat) => (console.log(dat.age))) //grabbed each individual age in a Map
 
-
-// let fun = 
+// let fun =
 // {
 //     name: "Mai",
 //     age: new Map('age', 14)
@@ -177,14 +166,12 @@ console.log(bubbleSort(data))
 //     name: "John",
 //     age: new Map('age', 15)
 // }
-;
 // for (let [key,value] of age.entries()) {
 //     console.log(key,value);
 // } => results: age 88
 
-
 // for (const value of age.values()) {
 //     console.log(value);
-// } 
+// }
 
 // console.log(Object.getEntries(fun.entries()))
---
+++ week4bonus.js
@@ -0,0 +1,26 @@
+//capitalizeFirstLetter
+
+
+//iterative approach
+// const capitalizeFirstLetter = (array) => {
+//     return array.map(element => element.charAt(0).toUpperCase() + element.slice(1));
+// }
+
+
+//recursive approach
+const capitalizeFirstLetter = (array) => {
+//edge case - if array is empty, return -1;
+if (!array.length) return -1;
+
+//base case - if array has one element, grab the first character and uppercase, then add rest of the words
+    if (array.length === 1) return array[0][0].toUpperCase() + array[0].slice(1);
+    
+//recursive case - recall the function
+//if array is not capitalize, grab the first letter of each word
+let res = capitalizeFirstLetter(array.slice(0, -1));
+  res.push(array.slice(array.length - 1)[0][0].toUpperCase());
+  return res;
+
+ }
+
+console.log(capitalizeFirstLetter(["mai","vang"]))
