c1e27e09cac4d701199cb0160b1c24fabced0e46
Mai Vang <vmaineng@gmail.com>
2022-12-12 15:04:33 -0800

    access

--- week5.js
+++ week5.js
@@ -1,8 +1,53 @@
 //problem 1 merge sort
 
+const data = {
+  results: {
+    payload: [
+      {
+        profile: {
+          name: "Tony",
+          rank: 9,
+          favorites: [
+            {
+              title: "Spider-Man",
+              rating: 5,
+            },
+            {
+              title: "Spongebob",
+              rating: 9,
+            },
+          ],
+        },
+      },
+      {
+        profile: {
+          name: "John",
+          rank: 2,
+          favorites: [
+            {
+              title: "Hulk",
+              rating: 1,
+            },
+            {
+              title: "Top Gun",
+              rating: 8,
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+
+
+console.log(data["results"].payload["profile"]);
+
+//merge sort by the rank
+
+
+//merge sort by the ratings and rank
+
 
 //problem 2 singly linked list
 
-
 //problem 3
-
