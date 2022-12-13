c8669c6f358f8166e52e26fb653741851fd94cdb
Mai Vang <vmaineng@gmail.com>
2022-12-13 12:07:03 -0800

    trying

--- week5.js
+++ week5.js
@@ -40,9 +40,22 @@ const data = {
 };
 
 
-console.log(data["results"].payload["profile"]);
+//console.log(data["results"]["payload"][1]["profile"]["rank"]); //how to get rank of 2 for John
+//console.log(data.results.payload) - gets to array of object filled with profiile
+
+//console.log(data["results"]["payload"][1]["profile"].favorites[0].rating); //got rating for Hulk for John
+
+const fakeData = {name: "mai"}
 
 //merge sort by the rank
+const mergeSortRank = (obj) => {
+
+// edge case - check if obj's length is <= 1 b/c can assume it's already sorted, return obj as is
+if (Object.keys(obj).length <= 1) return obj;
+}
+
+console.log(mergeSortRank(fakeData))
+
 
 
 //merge sort by the ratings and rank
